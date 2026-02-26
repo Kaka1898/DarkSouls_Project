683e97bca813c6e17ec006f33d59c23ccf9e7944
Kaka1898 <Kaka1898@users.noreply.github.com>
2026-02-26 23:00:07 +0900

    Fix: Resolve Three.js example module paths

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import * as THREE from 'three';
-import Stats from 'three/examples/jsm/libs/stats.module.js';
+import Stats from 'three/addons/libs/stats.module.js';
 import {
   scene, renderer, camera, spotLight, dirLight, composer,
   fogGateGroup, distXZ, updateWorldEffects,
